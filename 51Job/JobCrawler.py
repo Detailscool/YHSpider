015578df4c0afa88a64b697f83a855f0099a048b
Detailscool <496389006@qq.com>
2017-05-04 15:38:40 +0800

    update

--- 51Job/JobCrawler.py
+++ 51Job/JobCrawler.py
@@ -20,14 +20,16 @@ class JobCrawler(object):
             cls._instance = super(JobCrawler, cls).__new__(cls, *args, **kw)
         return cls._instance
 
-    driver = webdriver.PhantomJS()
-    f = open(u'前程无忧%s.csv' % (datetime.datetime.now().strftime('%Y%m%d%H:%M:%S')), 'wb')
-    f.write(unicode('\xEF\xBB\xBF', 'utf-8'))
-    writer = csv.writer(f)
-    writer.writerow(['公司', '公司性质', '公司人数', '行业', '职位', '学历', '区域', '薪酬', '发布时间', '链接'])
-    current_company = ''
+    __driver = webdriver.PhantomJS()
 
-    def get_job(self, soup):
+    __f = open(u'前程无忧%s.csv' % (datetime.datetime.now().strftime('%Y%m%d%H%M%S')), 'wb')
+    __f.write(unicode('\xEF\xBB\xBF', 'utf-8'))
+    __writer = csv.writer(__f)
+    __writer.writerow(['公司', '公司性质', '公司人数', '行业', '职位', '学历', '区域', '薪酬', '发布时间', '链接'])
+
+    __current_company = ''
+
+    def __get_job(self, soup):
         start = time.time()
 
         # print 'soup.prettify :', soup.prettify()
@@ -37,13 +39,13 @@ class JobCrawler(object):
         company_info = []
         for name, scale in zip(company_name, company_scale):
             name = name.get_text()
-            if self.current_company != name:
-                self.current_company = name
+            if self.__current_company != name:
+                self.__current_company = name
                 company_info.append(name)
                 for a in scale.get_text().split('|'):
                     company_info.append(a.strip())
         if company_info:
-            self.writer.writerow(company_info)
+            self.__writer.writerow(company_info)
         # return
 
         job_list = soup.select('div.dw_table')
@@ -56,7 +58,7 @@ class JobCrawler(object):
             job_time = job.find_all(name='span', class_='t5')
             for title, degree, area, salary, distribute_time in zip(job_titles, job_degrees, job_areas, job_salaries, job_time):
                 # print title.get_text(), '-', degree.get_text(), '-', area.get_text(), '-', salary.get_text(), '-',distribute_time.get_text(), '-', title.attrs['href']
-                self.writer.writerow(['', '', '', '', title.get_text(), degree.get_text(), area.get_text(), salary.get_text(),distribute_time.get_text(), title.attrs['href']])
+                self.__writer.writerow(['', '', '', '', title.get_text(), degree.get_text(), area.get_text(), salary.get_text(),distribute_time.get_text(), title.attrs['href']])
 
         current_page = soup.select('li.on')
         next_page = soup.select('li.bk > a')
@@ -75,25 +77,25 @@ class JobCrawler(object):
         if current_page==next_page:
             return
         else:
-            next = self.driver.find_elements_by_link_text('下一页')
+            next = self.__driver.find_elements_by_link_text('下一页')
             # print 'next: ', next, len(next)
             if next:
                 next[-1].click()
                 time.sleep(1)
-                bs = BeautifulSoup(self.driver.page_source, 'lxml')
-                self.get_job(bs)
+                bs = BeautifulSoup(self.__driver.page_source, 'lxml')
+                self.__get_job(bs)
 
     def get_job_info(self, url):
         header = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'
         }
 
-        self.driver.get(url)
+        self.__driver.get(url)
 
         wbdata = requests.get(url, headers=header)
         wbdata.encoding ='gb18030'
         soup = BeautifulSoup(wbdata.text, 'html.parser')
-        self.get_job(soup)
+        self.__get_job(soup)
 
 # Test
 # a = JobCrawler()
