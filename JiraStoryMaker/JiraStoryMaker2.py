1654b7de5ebfb22b5e1f82725b0ea2f9f075bbb8
李远晖 <henryli@kugou.net>
2018-07-14 10:11:52 +0800

    update

--- JiraStoryMaker/JiraStoryMaker2.py
+++ JiraStoryMaker/JiraStoryMaker2.py
@@ -9,45 +9,63 @@ import sys
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import os
+import json
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-def create_story(summary_text, work_time_text, REQ=None):
+def create_story(**kwargs):
+    summary_text = kwargs.get('summary_text', None)
+    work_time_text = kwargs.get('work_time_text', None)
+    REQ = kwargs.get('REQ', None)
+
     new_button = driver.find_element_by_id('create_link')
     new_button.click()
     time.sleep(0.5)
 
-    drop_menus = driver.find_elements_by_class_name('drop-menu')
-    if len(drop_menus) < 6:
-        print '出错啦'
-        sys.exit(1)
+    drop_menus = driver.find_elements_by_class_name('aui-ss-icon')
 
     project = drop_menus[0]
     project.click()
-    time.sleep(0.5)
 
     data_suggestions = driver.find_element_by_id('project-options').get_attribute('data-suggestions')
-    import json
     items = json.loads(data_suggestions)
+    # print items
     if isinstance(items, list) and items and isinstance(items[0], dict) and isinstance(items[0]['items'], list) and items[0]['items'] and isinstance(items[0]['items'][0], dict) and items[0]['items'][0]['label']:
         select_group = items[0]['items'][0]['label']
         if u'IOSZHIBO' not in select_group:
             groups = [a for a in driver.find_elements_by_css_selector('li a.aui-list-item-link') if 'IOSZHIBO' in a.text]
+            # print '\ngroups:', groups
             if groups:
                 groups[0].click()
+                print 'click'
                 time.sleep(0.5)
+        else:
+            project.click()
+
+    story_type = driver.find_element_by_id('issuetype-single-select')
+    story_type.click()
+    story_type_groups = [a for a in driver.find_elements_by_css_selector('li a.aui-list-item-link') if u'故事'==a.text]
+    if story_type_groups:
+        story_type_groups[0].click()
+        time.sleep(0.5)
+
+    drop_menus = driver.find_elements_by_class_name('aui-ss-icon')
+    if len(drop_menus) < 5:
+        time.sleep(10)
+        print '出错啦'
+        sys.exit(1)
 
     summary = driver.find_element_by_id('summary')
     summary.send_keys(unicode(summary_text))
     time.sleep(0.5)
 
-    sprint = drop_menus[5]
+    sprint = drop_menus[4]
     sprint.click()
     sprint_groups = [a for a in driver.find_elements_by_css_selector('li a.aui-list-item-link') if u'iOS直播服务组' in a.text]
     if sprint_groups:
-        sprint_groups[-1].click()
+        sprint_groups[0].click()
         time.sleep(0.5)
 
     test_type = Select(driver.find_element_by_id('customfield_10200'))
@@ -60,20 +78,33 @@ def create_story(summary_text, work_time_text, REQ=None):
 
     work_time = driver.find_element_by_id('customfield_10833')
     work_time.send_keys(work_time_text)
-    time.sleep(0.5)
 
-    code = driver.find_element_by_id('customfield_10503-3')
-    code.click()
+    # code = driver.find_element_by_id('customfield_10503-3')
+    # code.click()
 
     if REQ:
         question = driver.find_element_by_id('issuelinks-issues-textarea')
         question.send_keys(unicode(REQ))
 
+    items = driver.find_elements_by_css_selector('li.menu-item')
+    if items and len (items) > 1:
+        relationship_item = items[1]
+        relationship_item.click()
+        time.sleep(0.5)
+
+        dev_person = driver.find_element_by_css_selector('#customfield_10300_container textarea')
+        if dev_person and login_token.split('-'):
+            dev_person.send_keys(login_token.split('-')[0])
+            time.sleep(0.5)
+
     submit = driver.find_element_by_id('create-issue-submit')
     submit.click()
     time.sleep(2)
 
-    print '已建: ', summary_text, ', 时长， :', work_time_text, '天'
+    story = driver.find_element_by_css_selector('#aui-flag-container div div a')
+    story_href = story.get_attribute('href')
+    print summary_text, ': ', story_href
+    # print '已建: ', summary_text, ', 时长， :', work_time_text, '天'
 
 
 if __name__ == '__main__':
@@ -99,7 +130,7 @@ if __name__ == '__main__':
     # chrome_options.add_argument('--headless')
     driver = webdriver.Chrome(chrome_options=chrome_options)
 
-    url = ''
+    url = '' + login_token
 
     driver.get(url)
     # print driver.get_cookies()
